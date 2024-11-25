dcb09d18a1840099054eae8776f17765e2b7912f
joermartin <joermartin@gmail.com>
2024-11-24 21:05:36 -0500

    Update Package.swift
    Support back to iOS 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Markdownosaur",
-    platforms: [.iOS(.v15), .tvOS(.v15)],
+    platforms: [.iOS(.v12), .tvOS(.v15)],
     products: [
         .library(
             name: "Markdownosaur",
